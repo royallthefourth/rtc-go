e10ee813f428d77cd3789c1571c44199cfd0394c
Royall Spence <royall@royall.us>
2019-07-06 14:07:53 -0400

    Add PPM header output

--- canvas_test.go
+++ canvas_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestCanvas_GetPixel(t *testing.T) {
 	c := newCanvas(10, 20)
@@ -11,5 +14,24 @@ func TestCanvas_GetPixel(t *testing.T) {
 	c.SetPixel(1, 1, tuple{1, 0, 0, 0})
 	if !c.GetPixel(1, 1).Equals(tuple{1, 0, 0, 0}) {
 		t.Errorf(`Expected black but got %+v`, c.GetPixel(1, 1))
+	}
+}
+
+func TestCanvas_Ppm(t *testing.T) {
+	b := strings.Builder{}
+	c := newCanvas(5,3)
+	ch := make(chan string)
+	go c.Ppm(ch)
+	for line := range ch {
+		_, err := b.WriteString(line)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	lines := strings.Split(b.String(), "\n")
+
+	if lines[0] != `P3` || lines[2] != `255` {
+		t.Errorf(`Incorrect PPM header %s`, b.String())
 	}
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
 module raytrace
 
 go 1.12
-
-require (
-	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7 // indirect
-	github.com/tfriedel6/canvas v0.9.0
-	golang.org/x/image v0.0.0-20190523035834-f03afa92d3ff // indirect
-)
