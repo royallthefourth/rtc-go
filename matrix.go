c413795161200d0997290aaa0fbaa4c4b0340c34
Royall Spence <royall@royall.us>
2019-07-06 21:19:20 -0400

    Add more matrix funcs

--- matrix.go
+++ matrix.go
@@ -2,6 +2,12 @@ package main
 
 type matrix [][]float64
 
+var identity = matrix{
+	{1, 0, 0, 0},
+	{0, 1, 0, 0},
+	{0, 0, 1, 0},
+	{0, 0, 0, 1}}
+
 // newMatrix returns an initialized matrix
 func newMatrix(rows int, cols int) matrix {
 	m := make(matrix, rows)
@@ -24,4 +30,41 @@ func (m matrix) Equals(n matrix) bool {
 	}
 
 	return true
+}
+
+func (m matrix) Mul(n matrix) matrix {
+	out := newMatrix(len(m), len(n[0]))
+
+	for i := 0; i < len(out); i++ {
+		for j := 0; j < len(out[0]); j++ {
+			out[i][j] = m[i][0]*n[0][j] +
+				m[i][1]*n[1][j] +
+				m[i][2]*n[2][j] +
+				m[i][3]*n[3][j]
+		}
+	}
+
+	return out
+}
+
+func (m matrix) MulTuple(t tuple) tuple {
+	o := m.Mul(matrix{{t.X}, {t.Y}, {t.Z}, {t.W}})
+	return tuple{
+		X: o[0][0],
+		Y: o[1][0],
+		Z: o[2][0],
+		W: o[3][0],
+	}
+}
+
+func (m matrix) Transpose() matrix {
+	out := newMatrix(len(m[0]), len(m))
+
+	for i := 0; i < len(m); i++ {
+		for j := 0; j < len(m[0]); j++ {
+			out[j][i] = m[i][j]
+		}
+	}
+
+	return out
 }
--- matrix_test.go
+++ matrix_test.go
@@ -15,3 +15,61 @@ func TestMatrix_Equals(t *testing.T) {
 		t.Errorf(`%v should not equal %v`, m, o)
 	}
 }
+
+func TestMatrix_Mul(t *testing.T) {
+	a := matrix{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 8, 7, 6},
+		{5, 4, 3, 2}}
+	b := matrix{
+		{-2, 1, 2, 3},
+		{3, 2, 1, -1},
+		{4, 3, 6, 5},
+		{1, 2, 7, 8}}
+	prod := matrix{
+		{20, 22, 50, 48},
+		{44, 54, 114, 108},
+		{40, 58, 110, 102},
+		{16, 26, 46, 42}}
+
+	if !a.Mul(b).Equals(prod) {
+		t.Errorf(`Expected %v but got %v`, prod, a.Mul(b))
+	}
+}
+
+func TestMatrix_MulTuple(t *testing.T) {
+	a := matrix{
+		{1, 2, 3, 4},
+		{2, 4, 4, 2},
+		{8, 6, 4, 1},
+		{0, 0, 0, 1}}
+	b := tuple{
+		X: 1,
+		Y: 2,
+		Z: 3,
+		W: 1,
+	}
+	c := a.MulTuple(b)
+
+	if !c.Equals(tuple{18, 24, 33, 1}) {
+		t.Errorf(`Expected 18,24,33,1 but got %v`, c)
+	}
+}
+
+func TestMatrix_Transpose(t *testing.T) {
+	a := matrix{
+		{0, 9, 3, 0},
+		{9, 8, 0, 8},
+		{1, 8, 5, 3},
+		{0, 0, 5, 8}}
+	b := matrix{
+		{0, 9, 1, 0},
+		{9, 8, 8, 0},
+		{3, 0, 5, 5},
+		{0, 8, 3, 8}}
+
+	if !a.Transpose().Equals(b) {
+		t.Errorf(`Expected %v but got %v`, b, a.Transpose())
+	}
+}
